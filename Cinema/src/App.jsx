96b455416bde9d3fcb868eb277dd0c47f9551c9d
GTYaseen <128807198+GTYaseen@users.noreply.github.com>
2023-11-14 12:44:44 +0300

    Update App.jsx

--- Cinema/src/App.jsx
+++ Cinema/src/App.jsx
@@ -2,10 +2,6 @@ import "./App.css";
 import Header from "./components/NavBar/header.jsx";
 import Main from "./components/Main/Main.jsx";
 import Mid from "./components/Mid/card.jsx";
-const style = {
-  background: "#0092ff",
-  padding: "8px 0",
-};
 function App() {
   return (
     <div className="App">
