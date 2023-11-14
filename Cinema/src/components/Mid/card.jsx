236e0a43f3fe348fd1c0c785532cc3e43488c41e
GTYaseen <128807198+GTYaseen@users.noreply.github.com>
2023-11-14 14:44:33 +0300

    After

--- Cinema/src/components/Mid/card.jsx
+++ Cinema/src/components/Mid/card.jsx
@@ -2,173 +2,45 @@ import React from "react";
 import { Card } from "antd";
 import "./card.css";
 const { Meta } = Card;
-function card() {
+
+const cardData = [
+  {
+    title: "MovieName1",
+    date: "2020/4/21",
+    imdbRating: 7.8,
+    imageUrl: "https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png",
+  },
+  // Add more objects for each card
+  // {
+  //   title: "MovieName2",
+  //   date: "2020/4/22",
+  //   imdbRating: 8.0,
+  //   imageUrl: "https://example.com/image2.jpg",
+  // },
+  // ...
+];
+
+function CardComponent() {
   return (
     <div className="Mcard">
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
-      <Card hoverable
-        style={{ width: 240 ,border:"none"}}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="MovieName"/>
-        <div className="CB">
-          <p>2020/4/21</p>
-          <p>
-            <span>IMDB</span>
-            7.8
-          </p>
-        </div>
-      </Card>
+      {cardData.map((card, index) => (
+        <Card
+          key={index}
+          hoverable
+          style={{ width: 240, border: "none" }}
+          cover={<img alt="example" src={card.imageUrl} />}
+        >
+          <Meta title={card.title} />
+          <div className="CB">
+            <p>{card.date}</p>
+            <p>
+              <span>IMDB</span> {card.imdbRating}
+            </p>
+          </div>
+        </Card>
+      ))}
     </div>
   );
 }
 
-export default card;
+export default CardComponent;
